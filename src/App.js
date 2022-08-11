68c01c1d5adc73c5aebccfc848026a124b6086b9
anacgms <acgomes43@gmail.com>
2022-08-11 14:30:31 -0300

    arrowfunction

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+class App extends Component {
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+ mensagem = () => {
+     return <h2>Hello world, I'm a cherry bomb</h2>
+   } 
+
+   escolhaMensagem = (msg) => {
+     return <h2>{msg}</h2>
+   }
+
+   dobro = (numero) => {
+     return <h2>O dobro do número: {numero * 2}</h2>
+   }
+
+   //numero = (num) => {
+     //if(num >= 0){
+       //return <h3>Número positivo</h3>
+     //}else{
+       //return <h3>Número negativo</h3>
+    // }
+   //}
+
+   render(){
+    return(
+    <div>
+      {this.mensagem()}
+      {this.dobro(10)}
     </div>
-  );
-}
+    )
+   }
+  }
 
-export default App;
+   export default App
